5f722320a6a26d765669c51f04aa7922f5b7011a
Mohammad-Rahim <mailmr.rahim@gmail.com>
2021-06-06 18:58:50 +0545

    Use of simple css

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../styles/components/Header.css'
+
+function header() {
+      return (
+          <div>
+          
+            <div className="logo">Dashboard</div>
+        
+          </div>
+      )
+  }
+  
+  export default header
+  
+
+
+
